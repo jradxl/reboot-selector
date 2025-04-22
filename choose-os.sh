1d3bb299f1321abf5f126e27aa2aa3be5a1b7a48
John Radley <jradxl@gmail.com>
2025-04-22 18:00:05 +0200

    Updating script

--- choose-os.sh
+++ choose-os.sh
@@ -22,7 +22,7 @@ do
             exit
             break
             ;;
-        *) echo "invalid option $REPLY";;
+        *) echo "invalid option";;
     esac
 done
 
@@ -32,7 +32,7 @@ echo "Required Server is: $ID2"
 if [[ $ID == "opensuse-tumbleweed" ]]; then
     echo "Running on Tumbleweed: Easy as chroot not needed."
 
-    if [[ $ID2 == opensuse-tumbleweed"" ]]; then
+    if [[ $ID2 == "opensuse-tumbleweed" ]]; then
         sed -i 's/GRUB_DEFAULT=[0-9]*/GRUB_DEFAULT=0/g' /etc/default/grub
         echo "Rebooting into Tumbleweed ..."
     fi
@@ -45,8 +45,42 @@ if [[ $ID == "opensuse-tumbleweed" ]]; then
 fi
 
 if [[ $ID == "ubuntu" ]]; then
-    echo "Running on Ubuntu: Need to chroot to update /dev/sda"
+    echo "Running on Ubuntu: Need to chroot to update /dev/sdb2"
+    mountpoint -q /mnt/disk1
+    ret="$?"
+    if [[ "$ret" ==  0 ]]; then
+        echo "Mounted ..."
+    else
+        echo "Not Yet Mounted ..."
+        mount /dev/sdb2 /mnt/disk1/
+    fi
+
+    if [[ $ID2 == "opensuse-tumbleweed" ]]; then
+        sed -i 's/GRUB_DEFAULT=[0-9]*/GRUB_DEFAULT=0/g' /mnt/disk1/etc/default/grub
+        echo "Rebooting into Tumbleweed ..."
+    fi
+
+    if [[ $ID2 == "ubuntu" ]]; then
+        sed -i 's/GRUB_DEFAULT=[0-9]*/GRUB_DEFAULT=2/g' /mnt/disk1/etc/default/grub
+        echo "Rebooting into Ubuntu ..."
+    fi
+
+    mount --bind /proc /mnt/disk1/proc/
+    mount --bind /dev/ /mnt/disk1/dev/
+    mount --bind /sys/ /mnt/disk1/sys/
+
+    chroot /mnt/disk1 /bin/bash -x <<'EOF'
+    sleep 2
+    /sbin/update-bootloader
+    sleep 2
+EOF
+    echo "Finished Chroot..."
+    umount /mnt/disk1/proc/
+    umount /mnt/disk1/dev/
+    umount /mnt/disk1/sys/
+    umount /mnt/disk1
 fi
 
 sleep 2
+echo "Rebooting as requested ..."
 reboot
